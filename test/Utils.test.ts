7b014bf6fa461070c91f900d847ec345872ffc5d
Tamas <tmajor.consulting@gmail.com>
2022-02-17 20:02:45 +0100

    TDD second extra test green

--- test/Utils.test.ts
+++ test/Utils.test.ts
@@ -3,13 +3,25 @@ import { Utils } from "../src/Utils";
 describe('Utils', () => {
     it('should return uppercase string', function () {
         const result = Utils.toUpperCase('testString');
+
         expect(result).toEqual('TESTSTRING');
     });
 
     it('should parse simple url', function () {
         const parsedUrl = Utils.parseUrl('http://localhost:1337/login');
+
         expect(parsedUrl.href).toEqual('http://localhost:1337/login')
         expect(parsedUrl.port).toEqual('1337')
         expect(parsedUrl.protocol).toEqual('http:')
+    });
+
+    it('should parse rul with query', function () {
+        const parsedUrl = Utils.parseUrl('http://localhost:1337/login?user=user&pass=pass');
+        const expectedQuery = {
+            user: 'user',
+            pass: 'pass'
+        }
+
+        expect(parsedUrl.query).toEqual(expectedQuery);
     });
 })
