deb0363b3a91d2b2dc254e283275ffa719ded98a
Tamas <tmajor.consulting@gmail.com>
2022-02-17 19:58:00 +0100

    TDD second

--- src/Utils.ts
+++ src/Utils.ts
@@ -2,4 +2,8 @@ export class Utils {
     public static toUpperCase(arg: string): string {
         return arg.toUpperCase();
     }
+
+    public static parseUrl(url: string): any {
+        return '';
+    }
 }
--- test/Utils.test.ts
+++ test/Utils.test.ts
@@ -5,4 +5,11 @@ describe('Utils', () => {
         const result = Utils.toUpperCase('testString');
         expect(result).toEqual('TESTSTRING');
     });
+
+    it('should parse simple url', function () {
+        const parsedUrl = Utils.parseUrl('http://localhost:1337/login');
+        expect(parsedUrl.href).toEqual('http://localhost:1337/login')
+        expect(parsedUrl.port).toEqual('1337')
+        expect(parsedUrl.protocol).toEqual('http:')
+    });
 })
