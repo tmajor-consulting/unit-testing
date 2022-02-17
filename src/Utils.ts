6a5f1d0feedc70641a9d77b37f5811164629f51e
Tamas <tmajor.consulting@gmail.com>
2022-02-17 19:58:40 +0100

    TDD second green

--- src/Utils.ts
+++ src/Utils.ts
@@ -1,9 +1,11 @@
+import {parse, UrlWithParsedQuery} from "url";
+
 export class Utils {
     public static toUpperCase(arg: string): string {
         return arg.toUpperCase();
     }
 
-    public static parseUrl(url: string): any {
-        return '';
+    public static parseUrl(url: string): UrlWithParsedQuery {
+        return parse(url, true);
     }
 }
