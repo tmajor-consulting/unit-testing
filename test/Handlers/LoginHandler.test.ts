cff389004e4977e5fdc3b64e189019086e65b6f6
Tamas <tmajor.consulting@gmail.com>
2022-02-20 14:06:02 +0100

    basic mock

---
+++ test/Handlers/LoginHandler.test.ts
@@ -0,0 +1,32 @@
+import {LoginHandler} from "../../src/Handlers/LoginHandler";
+import {HTTP_CODES, HTTP_METHODS} from "../../src/Domain/Server";
+
+describe('LoginHandler', () => {
+
+    describe('handleRequest', () => {
+        let loginHandler: LoginHandler;
+
+        const requestMock = {
+            method: ''
+        };
+        const responseMock = {
+            writeHead: jest.fn()
+        };
+        const authorizerMock = {};
+
+        beforeEach(() => {
+            loginHandler = new LoginHandler(
+                requestMock as any, // DO NOT USE THIS OUTSIDE OF TESTS
+                responseMock as any,
+                authorizerMock as any
+            )
+        });
+
+        it('should respond with OK for OPTIONS call', async() => {
+            requestMock.method = HTTP_METHODS.OPTIONS;
+            await loginHandler.handleRequest();
+            expect(responseMock.writeHead).toHaveBeenCalledTimes(1);
+            expect(responseMock.writeHead).toHaveBeenCalledWith(HTTP_CODES.OK);
+        });
+    })
+})
