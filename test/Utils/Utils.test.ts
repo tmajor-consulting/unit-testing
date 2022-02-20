d5726b9359319b7a027229b59fcc2ac5dcd8c4b4
Tamas <tmajor.consulting@gmail.com>
2022-02-20 10:42:13 +0100

    why we need stubs; refactor utils.test to new structure;

--- test/Utils/Utils.test.ts
+++ test/Utils/Utils.test.ts
@@ -1,31 +1,48 @@
 import { Utils } from "../../src/Utils/Utils";
+import {IncomingMessage} from "http";
 
 describe('Utils', () => {
-    it('should return uppercase string', function () {
-        const result = Utils.toUpperCase('testString');
 
-        expect(result).toEqual('TESTSTRING');
-    });
+    describe('toUpperCase', () => {
+        it('should return uppercase string', function () {
+            const result = Utils.toUpperCase('testString');
 
-    it('should parse simple url', function () {
-        const parsedUrl = Utils.parseUrl('http://localhost:1337/login');
+            expect(result).toEqual('TESTSTRING');
+        });
+    })
 
-        expect(parsedUrl.href).toEqual('http://localhost:1337/login')
-        expect(parsedUrl.port).toEqual('1337')
-        expect(parsedUrl.protocol).toEqual('http:')
-    });
+    describe('parseUrl', () => {
+        it('should parse simple url', function () {
+            const parsedUrl = Utils.parseUrl('http://localhost:1337/login');
 
-    it('should parse rul with query', function () {
-        const parsedUrl = Utils.parseUrl('http://localhost:1337/login?user=user&pass=pass');
-        const expectedQuery = {
-            user: 'user',
-            pass: 'pass'
-        }
+            expect(parsedUrl.href).toEqual('http://localhost:1337/login')
+            expect(parsedUrl.port).toEqual('1337')
+            expect(parsedUrl.protocol).toEqual('http:')
+        });
 
-        expect(parsedUrl.query).toEqual(expectedQuery);
-    });
+        it('should parse rul with query', function () {
+            const parsedUrl = Utils.parseUrl('http://localhost:1337/login?user=user&pass=pass');
+            const expectedQuery = {
+                user: 'user',
+                pass: 'pass'
+            }
 
-    it('should throw error on empty url', function () {
-        expect(() => Utils.parseUrl('')).toThrowError('Empty URL')
-    });
+            expect(parsedUrl.query).toEqual(expectedQuery);
+        });
+
+        it('should throw error on empty url', function () {
+            expect(() => Utils.parseUrl('')).toThrowError('Empty URL')
+        });
+    })
+
+    describe('getRequestBasePath', () => {
+        it('should return base path for a valid request',  () => {
+            const url = 'http://localhost:8080/login';
+
+            const result = Utils.getRequestBasePath(url);
+
+            expect(result).toEqual('login');
+        });
+
+    })
 })
