712d36c223572c8ca168a3a5541db0a8c3f09e82
Tamas <tmajor.consulting@gmail.com>
2022-02-20 14:23:40 +0100

    brakepoint on LoginHandler:37 for debug

--- test/Handlers/LoginHandler.test.ts
+++ test/Handlers/LoginHandler.test.ts
@@ -1,5 +1,6 @@
 import {LoginHandler} from "../../src/Handlers/LoginHandler";
 import {HTTP_CODES, HTTP_METHODS} from "../../src/Domain/Server";
+import {Utils} from "../../src/Utils/Utils";
 
 describe('LoginHandler', () => {
 
@@ -14,12 +15,15 @@ describe('LoginHandler', () => {
         };
         const authorizerMock = {};
 
+        const getRequestBodyMock = jest.fn();
+
         beforeEach(() => {
             loginHandler = new LoginHandler(
                 requestMock as any, // DO NOT USE THIS OUTSIDE OF TESTS
                 responseMock as any,
                 authorizerMock as any
             )
+            Utils.getRequestBody = getRequestBodyMock;
         });
 
         afterEach(() => {
@@ -38,6 +42,17 @@ describe('LoginHandler', () => {
             await loginHandler.handleRequest();
 
             expect(responseMock.writeHead).not.toHaveBeenCalled();
+        });
+
+        it.only('should respond with token on valid POST request', async () => {
+            requestMock.method = HTTP_METHODS.POST;
+
+            getRequestBodyMock.mockReturnValueOnce({
+                username: 'mockedUsername',
+                password: 'mockedPassword'
+            })
+            await loginHandler.handleRequest();
+
         });
     })
 })
